c9bd708ea53d0dc616b8146893a509f0d57a8d9a
ReziMi <revaz.mikadze92@gmail.com>
2024-05-18 02:54:32 +0400

    initializing basics for tic tac toe game

---
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './TicTacToe.css'
+import circle_icon from '../Assets/circle.png'
+import cross_icon from '../Assets/cross.png'
+
+function TicTacToe() {
+  return (
+    <div className='container'>
+      <h1 className='title'>Tic Tac Toe Game in <span>React</span></h1>
+
+      <div className='board'>
+
+      </div>
+
+      <button className="reset">Reset</button>
+    </div>
+  )
+}
+
+export default TicTacToe
